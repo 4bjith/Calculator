3f6b0634e17cb59544dac3baa4048f99eea3be79
4bjith <abhijithpops1890@gmail.com>
2025-07-18 10:26:26 +0530

    add key input function

--- script.js
+++ script.js
@@ -13,8 +13,28 @@ function clrDisplay(){
 }
 function eqlclick(){
     let text=document.getElementById("screen").value
-    let result=eval(text)
+    
+    let result=new Function('return '+text)()
     document.getElementById("screen").value=result
     isCalculated=true
 
 }
+// let display=document.getElementById("screen");
+function backspace(){
+let display=document.getElementById("screen");
+display.value=display.value.slice(0,-1)
+}
+
+document.addEventListener("keydown", function(event){
+    const key=event.key;
+    if(!isNaN(key) || "*/+-.".includes(key)){
+        btnclick(key);
+    }else if(key==="Enter" || key==="="){
+        event.preventDefault();
+        eqlclick();
+    }else if(key==="Backspace"){
+        backspace();
+    }else if (key.toLowerCase() === "c") {
+    clrDisplay();
+  }
+});
